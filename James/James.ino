08e64fb728ecd9f1238d2d66db91f30acf133271
Erik Wegner <e_wegner@web.de>
2015-07-05 21:26:24 +0200

    Paddle

--- James/James.ino
+++ James/James.ino
@@ -2,6 +2,35 @@
 #include "Adafruit_LEDBackpack.h"
 #include "Adafruit_GFX.h"
 
+#define LCD_PORT_TX 7
+#define LCD_PORT_RX 6 // not really used 
+#include <SoftwareSerial.h>
+#include <inttypes.h>
+class mySerial : public SoftwareSerial {
+
+  public:
+    mySerial(uint8_t receivePin, uint8_t transmitPin,
+      bool inverse_logic = false) :
+      SoftwareSerial(receivePin, transmitPin,inverse_logic) {}
+
+    virtual size_t write(uint8_t byte) {
+      return SoftwareSerial::write(byte);
+    }
+
+    int printf(char* fmt, ...) {
+      char buff[256];
+      va_list args;
+      va_start(args, fmt);
+      int return_status = vsnprintf(buff, sizeof(buff), fmt, args);
+      va_end(args);
+      uint8_t *s = (uint8_t *)&buff;
+      while (*s) write(*s++);
+      return return_status;
+    }
+
+};
+
+mySerial lcdserial(LCD_PORT_RX, LCD_PORT_TX);
 Adafruit_BicolorMatrix matrix = Adafruit_BicolorMatrix();
 
 static const uint8_t PROGMEM
@@ -15,8 +44,33 @@ static const uint8_t PROGMEM
     B01000011,
     B00111100 };
 
+byte paddlePosition = 0;
+#define paddlePin 0
+
+#include <stdarg.h>
+void p(char *fmt, ... ){
+        char buf[128]; // resulting string limited to 128 chars
+        va_list args;
+        va_start (args, fmt );
+        vsnprintf(buf, 128, fmt, args);
+        va_end (args);
+        Serial.print(buf);
+}
 
 void setup() {
+  Serial.begin(115200);
+  
+  lcdserial.begin(9600);
+  lcdserial.write(254); // move cursor to beginning of first line
+  lcdserial.write(128);
+
+  lcdserial.print("                "); // clear display
+  lcdserial.print("                ");
+
+  lcdserial.write(254); // move cursor to beginning of first line
+  lcdserial.write(128);
+  
+  Serial.println(F("Ready"));
   // put your setup code here, to run once:
   matrix.begin(0x70);  // pass in the address
   
@@ -28,9 +82,8 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  tennis();
 }
-
 
 void intro() {
   matrix.drawLine(0,0,1,1, LED_GREEN);
@@ -65,4 +118,16 @@ void intro() {
     matrix.writeDisplay();
     delay(250);
   }
+  
+  matrix.clear();
+}
+
+
+void tennis() {
+  matrix.clear();
+  paddlePosition = map(analogRead(paddlePin), 0, 1023, 0, 6);
+  
+  // paint paddle
+  matrix.drawLine(0, paddlePosition, 0, paddlePosition + 1, LED_YELLOW);
+  matrix.writeDisplay();
 }
