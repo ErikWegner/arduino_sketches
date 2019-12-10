894f4edb8c32ee268156cfd5937db8b9d0a670ea
Erik Wegner <E_Wegner@web.de>
2019-12-10 22:24:07 +0100

    Bounce2 library, four buttons on ESP32

--- smarthome2/buttons.ino
+++ smarthome2/buttons.ino
@@ -1,10 +1,61 @@
+#include <Bounce2.h>
+
 #include "pinlayout.h"
 
+Bounce debouncerLinksHoch = Bounce();
+Bounce debouncerLinksRunter = Bounce();
+Bounce debouncerRechtsHoch = Bounce();
+Bounce debouncerRechtsRunter = Bounce();
+
 void setupGPIO() {
-  pinMode(BUTTONLINKSHOCH, INPUT);
-  pinMode(BUTTONRECHTSHOCH, INPUT);
-  pinMode(BUTTONLINKSRUNTER, INPUT);
-  pinMode(BUTTONRECHTSRUNTER, INPUT);
+  // ok: braun  schwarz rot    1000 Ohm
+  // ok: orange orange  orange 2200 Ohm
+  // ok: braun  schwarz ?
+  // ok: gelb   lila    rot    4600 Ohm
+  debouncerLinksHoch.attach(BUTTONLINKSHOCH, INPUT);
+  debouncerRechtsHoch.attach(BUTTONRECHTSHOCH, INPUT);
+  debouncerLinksRunter.attach(BUTTONLINKSRUNTER, INPUT);
+  debouncerRechtsRunter.attach(BUTTONRECHTSRUNTER, INPUT);
 
   pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void checkButtons() {
+  // Links hoch
+  debouncerLinksHoch.update();
+  if (debouncerLinksHoch.fell()) {
+    Serial.println("LH close");
+  }
+  if (debouncerLinksHoch.rose()) {
+    Serial.println("LH open");
+  }
+
+
+  // Rechts hoch
+  debouncerRechtsHoch.update();
+  if (debouncerRechtsHoch.fell()) {
+    Serial.println("RH close");
+  }
+  if (debouncerRechtsHoch.rose()) {
+    Serial.println("RH open");
+  }
+
+  // Links runter
+  debouncerLinksRunter.update();
+  if (debouncerLinksRunter.fell()) {
+    Serial.println("LR close");
+  }
+  if (debouncerLinksRunter.rose()) {
+    Serial.println("LR open");
+  }
+
+
+  // Rechts runter
+  debouncerRechtsRunter.update();
+  if (debouncerRechtsRunter.fell()) {
+    Serial.println("RR close");
+  }
+  if (debouncerRechtsRunter.rose()) {
+    Serial.println("RR open");
+  }
 }
--- smarthome2/smarthome2.ino
+++ smarthome2/smarthome2.ino
@@ -3,7 +3,6 @@
 volatile bool readButtons = false;
 volatile bool ledState = false;
 
-portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
 hw_timer_t * timer = NULL;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
@@ -12,8 +11,6 @@ void IRAM_ATTR onTimer() {
   ledState = !ledState;
   portEXIT_CRITICAL_ISR(&timerMux);
   digitalWrite(LED_BUILTIN, ledState ? HIGH : LOW);
-  Serial.print(ledState);
-  Serial.println("onTimer");
 }
 
 void setupTimer() {
@@ -23,28 +20,15 @@ void setupTimer() {
   timerAlarmEnable(timer);
 }
 
-void IRAM_ATTR handleInterrupt() {
-  portENTER_CRITICAL_ISR(&mux);
-  readButtons = true;
-  portEXIT_CRITICAL_ISR(&mux);
-}
-
-void readButtonsHandler() {
-  portENTER_CRITICAL(&mux);
-  readButtons = false;
-  portEXIT_CRITICAL(&mux);
-}
-
 void setup() {
   Serial.begin(115200);
   Serial.println(F("Booting"));
   setupGPIO();
-  attachInterrupt(digitalPinToInterrupt(BUTTONLINKSHOCH), readButtonsHandler, CHANGE);
   delay(1500);
   Serial.println(F("Starting timer"));
   setupTimer();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  checkButtons();
 }
